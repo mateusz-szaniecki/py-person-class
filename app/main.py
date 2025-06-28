09910bf7308b24ce713f7b54159f6018e5a22986
Mateusz <szanieckimateusz@gmail.com>
2025-06-28 12:44:49 +0200

    Completed create-person-list function

--- app/main.py
+++ app/main.py
@@ -8,4 +8,18 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
-    person = [Person(person["name"], person["age"]) for person in people]
+    for person in people:
+        Person(person["name"], person["age"])
+
+    for person in people:
+        person_instance = Person.people[person["name"]]
+        if "wife" in person and person["wife"]:
+            wife_name = person["wife"]
+            person_instance.wife = Person.people[wife_name]
+            Person.people[wife_name].husband = person_instance
+        if "husband" in person and person["husband"]:
+            husband_name = person["husband"]
+            person_instance.husband = Person.people[husband_name]
+            Person.people[husband_name].wife = person_instance
+
+    return [Person.people[person["name"]] for person in people]
