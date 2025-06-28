87f0df145eb5842c4434521105497807eecd8689
Mateusz <szanieckimateusz@gmail.com>
2025-06-28 12:10:14 +0200

    Implicating a Person Class

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
